3afb472f4ee6f7020563baa58e2443a14f8a96ba
unknown <lambangws21@gmail.com>
2023-08-06 11:41:41 +0700

    Ayo tambahkan DnD

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -8,61 +8,57 @@ import {
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-function CardItems(props) {
+function CardItems({ data }) {
   return (
     <div className="bg-white p-1 rounded-md shadow-md mt-3 flex flex-col">
-      <label className="bg-gradient-to-r from-blue-500 to-blue-200 px-2 py-1 rounded text-white text-sm">
-        Low Priority
-      </label>
+<label
+  className={`rounded px-2 py-1 text-white text-sm ${
+    data.priority === 0
+      ? 'bg-gradient-to-r from-blue-500 to-blue-200'
+      : data.priority === 1
+      ? 'bg-gradient-to-r from-green-500 to-green-200'
+      : 'bg-gradient-to-r from-red-600 to-red-300'
+  }`}
+>
+  {data.priority === 0
+    ? 'Low Priority'
+    : data.priority === 1
+    ? 'Medium Priority'
+    : 'High Priority'}
+</label>
+
       <h5 className="tex-md my-3 leading-4">Tindakan Operasi Laparatomie</h5>
       <div className="flex justify-between">
         <div className="flex space-x-4 items-center">
           <span className="flex space-x-2 items-center">
             <ChatBubbleLeftEllipsisIcon className="w-5 h-5 text-gray-500 mr-2" />
-            <span>2</span>
+            <span>{data.chat}</span>
           </span>
           <span className="flex space-x-2 items-center">
             <PaperAirplaneIcon className="w-5 h-5 text-gray-500 mr-2" />
-            <span>1</span>
+            <span>{data.Status}</span>
           </span>
         </div>
         <ul className="flex flex-row-reverse space-x-1 items-center">
           <button className="border border-dashed flex items-center  w-5 h-5 border-gray-500 rounded-full">
             <PlusIcon className="w-5 h-5 text-gray-500 items-center justify-center" />
           </button>
-          <li>
-            {" "}
-            <Image
-              src="https://randomuser.me/api/portraits/men/27.jpg"
-              width="36"
-              height="36"
-              objectFit="cover "
-              alt="user-team"
-              className="rounded-full"
-            />
-          </li>
-          <li>
-            {" "}
-            <Image
-              src="https://randomuser.me/api/portraits/men/22.jpg"
-              width="36"
-              height="36"
-              objectFit="cover "
-              alt="user-team"
-              className="rounded-full"
-            />
-          </li>
-          <li>
-            {" "}
-            <Image
-              src="https://randomuser.me/api/portraits/men/91.jpg"
-              width="36"
-              height="36"
-              objectFit="cover "
-              alt="user-team"
-              className="rounded-full"
-            />
-          </li>
+          {data.assignees.map((ass, index) => {
+            return (
+              <li key={index}>
+                <Image
+                  src={ass.avt}
+                  width={36}
+                  height={36}
+                  objectFit="cover"
+                  alt="user-team"
+                  className="rounded-full"
+                />
+              </li>
+            );
+          })}
+
+
         </ul>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,14 @@
 import Layout from "@/components/Layout";
 import { Inter } from "next/font/google";
 import {
-  ChatBubbleLeftEllipsisIcon,
   ChevronDownIcon,
   EllipsisVerticalIcon,
-  PaperAirplaneIcon,
   PlusIcon,
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import CardItems from "@/components/CardItems";
-import "../data/borad-data.json";
+import BoardData from "../data/borad-data.json";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -67,18 +66,35 @@ export default function Home() {
           </div>
         </div>
         {/* Board Columns */}
-        <div className="grid grid-cols-4 gap-5 my-5 ">
-          <div className="bg-gray-100 rounded-md shadow-md p-2 flex flex-col relative overflow-hidden">
-            <span className="w-full h-1 bg-gradient-to-r bg-pink-700 to-red-400 rounded-md absolute inset-x-0 top-0"></span>
-            <h4 className="flex justify-between items-center mb-2">
-              <span className="text-2xl text-gray-600 ">Backlog</span>
-              <EllipsisVerticalIcon className="h-5 w-5 text-gray-700" />
-            </h4>
-            <CardItems />
-            <CardItems />
-            <CardItems />
-          </div>
+      <Draggable>
+      <div className="grid grid-cols-4 gap-5 my-5 ">
+          { 
+          BoardData.map((board, index) => {
+            return (
+                <div
+                  key={index}
+                  className="bg-gray-100 rounded-md shadow-md p-2 flex flex-col relative overflow-hidden"
+                >
+                  <span className="w-full h-1 bg-gradient-to-r bg-pink-700 to-red-400 rounded-md absolute inset-x-0 top-0"></span>
+                  <h4 className="flex justify-between items-center mb-2">
+                    <span className="text-2xl text-gray-600 ">{board.name}</span>
+                    <EllipsisVerticalIcon className="h-5 w-5 text-gray-700" />
+                  </h4>
+                  {board.items.length > 0 && (
+                    board.items.map((item, iIndex) => {
+                      return <CardItems key={iIndex} data={item} />
+                    })
+                  )}
+                  <button className="flex justify-center items-center mt-6 space-x-2 mb-3 border p-1 rounded-md bg-slate-50 shadow-lg">
+                    <span className="font-semibold">Add Scedule</span>
+                    <PlusIcon className="w-5 h-5 text-gray-50 shadow-md bg-green-400 rounded-full" />
+
+                  </button>
+                </div>
+            );
+          })}
         </div>
+      </Draggable>
       </div>
     </Layout>
   );
